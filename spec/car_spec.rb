3f012994e219b6875a34194186b2ef6b17ba1c0e
iheartkode <markhjr@outlook.com>
2015-04-08 14:27:54 -0700

    Added test for initialize

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,27 +3,33 @@ require './car'
 
 describe Car do
     it "should make a new Car" do
-      car = Car.new
+      car = Car.new(:year, :make, :model)
+    end
+  end
+
+  describe "#initialize" do
+    it "sets the year make and model" do
+      car = Car.new(:year, :make, :model)
     end
   end
 
 describe "#horn" do
     it "should have a horn and return Beep Beep" do
-      car = Car.new
+      car = Car.new(:year, :make, :model)
       expect(car.horn).to eq "Beep Beep"
   end
 end
 
 describe "#start" do
   it "should start the car and return vroom vroom" do
-    car = Car.new
+    car = Car.new(:year, :make, :model)
     expect(car.start).to eq "vroom vroom"
   end
 end
 
 describe "#stop_engine" do
   it "should turn the cars engine off" do
-    car = Car.new
+    car = Car.new(:year, :make, :model)
     expect(car.stop_engine).to eq "the car comes to a halt"
   end
 end
